6f0b641c1659bc557304d5a132325dc780f1ca7b
Temi Makinde <Temi.Makinde@Gmail.com>
2019-06-30 13:42:58 +0100

    Removed sensitive information

--- connection/connect.js
+++ connection/connect.js
@@ -2,10 +2,10 @@ const sql = require("mssql");
 const config = require("../config.json");
 const connect = function() {
   const conn = new sql.ConnectionPool({
-    user: config.user, //"administrator",
-    password: config.password, //"LUL0BchAThePHVziQjP9$Y49wPb^2ABQI%d",
-    server: config.server, //"morla-dev.ct73sprczork.us-east-2.rds.amazonaws.com",
-    database: config.database //"morla-dev"
+    user: config.user,
+    password: config.password,
+    server: config.server,
+    database: config.database
   });
 
   return conn;
